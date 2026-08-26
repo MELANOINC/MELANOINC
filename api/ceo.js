3a1fd527b0c9d7b2dbf894005b8ee2230472fa80
Bruno Melano <melanobruno@gmail.com>
2026-08-26 20:45:58 -0300

    fix: point CEO endpoint to Vite public root

--- api/ceo.js
+++ api/ceo.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   res.setHeader('Cache-Control', 'public, max-age=300, s-maxage=86400');
-  res.redirect(307, '/assets/ceo-bruno-melano.webp');
+  res.redirect(307, '/ceo-bruno-melano.webp');
 }
